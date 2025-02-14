df798c542d06c1f760616f16db4057be00dd477d
TheSaucySerpent <sjburden2013@gmail.com>
2025-02-14 02:12:35 -0500

    Fix binding to Heroku port

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template
+import os
 
 app =  Flask(__name__)
 
@@ -7,4 +8,8 @@ def home():
   return render_template('index.html') # Render the index.html template
 
 if __name__ == "__main__": # check if the script is being run directly
-  app.run(debug=True) # Run the appication in debug mode
+  # app.run(debug=True) # Run the appication in debug mode
+  
+  # Use the PORT environment variable provided by Heroku
+  port = int(os.environ.get("PORT", 5000))
+  app.run(host='0.0.0.0', port=port)
